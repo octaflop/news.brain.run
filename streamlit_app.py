167df5e91669abcffdb4c5cafdbeb5a67e1a69c1
Faris Chebib <faris@theluckybead.com>
2024-01-01 20:40:18 -0700

    Remove pesky print statement, fix list.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ def get_article_data():
     db_name = os.environ.get("MD_DB_NAME", "athena_beta")
     motherduck_token = os.environ.get("MOTHERDUCK_KEY")
     db_url = f"md:{db_name}?motherduck_token={motherduck_token}"
-    print(db_url)
     conn = duckdb.connect(db_url)
 
     q = "SELECT title,category,summary,link,site_summary,authors FROM athena_beta.public.summarize_articles"
@@ -24,9 +23,6 @@ for idx, row in get_article_data().sort_index().iterrows():
     summary = row['summary']
     authors = row['authors']
 
-    # Displaying the link with clickable URL
-    st.markdown(f"# [{article_title}]({article_link})")
-    st.markdown(f"## {','.join(authors)}")
-
-    # Displaying the summary
-    st.write(f"Summary: {summary}")
+    st.markdown(f"[{article_title}]({article_link})")
+    st.write(authors)
+    st.write(summary)
