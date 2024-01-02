edcdeb172be6ba0fbc95225370581084b581e329
Faris Chebib <faris@theluckybead.com>
2024-01-01 20:34:06 -0700

    Update README for kickoff

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# news.brain.run
-Consolidation of ideas for brain running
+# 📰.🧠.🏃🏽‍♂️💨
+
+## news.brain.run
+
+A frontend for some AI ETL pipelines which grab articles from the [arxiv category taxonomy](https://arxiv.org/category_taxonomy) and summarizes them with various AI models. 
+
+
+## Dev Stuff
+
+### Setup
+
+- direnv allow
+- set a `MOTHERDUCK_KEY` and optionally a `MD_DB_NAME`
+
+### TODO
+
+- [x] Make a TODO
+- [x] Incorporate DuckDB / Motherduck
+- [ ] Add a field and tag for models used
+- [ ] Hoist this implementation onto a domain via github pages / subdomain CNAME stuff.
+
+
+## Misc
+
+More at [faris.ai](https://faris.ai)
+
+This project is a v1.4 of [news.faris.ai](https://news.faris.ai/)
--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ def get_article_data():
     print(db_url)
     conn = duckdb.connect(db_url)
 
-    # Assuming table name is "articles". Replace "articles" with your actual table name
     q = "SELECT title,category,summary,link,site_summary,authors FROM athena_beta.public.summarize_articles"
     df: pd.DataFrame = conn.execute(q).fetch_df()
 
@@ -20,12 +19,14 @@ def get_article_data():
 
 
 for idx, row in get_article_data().sort_index().iterrows():
-    article_title = row['title']
     article_link = idx
+    article_title = row['title']
     summary = row['summary']
+    authors = row['authors']
 
     # Displaying the link with clickable URL
-    st.markdown(f"[{article_title}]({article_link})")
+    st.markdown(f"# [{article_title}]({article_link})")
+    st.markdown(f"## {','.join(authors)}")
 
     # Displaying the summary
     st.write(f"Summary: {summary}")
